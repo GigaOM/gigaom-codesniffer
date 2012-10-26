89f14f3eea8dad347db4bc8c73cde9a980b4a7b2
Matthew Batchelder <borkweb@gmail.com>
2012-10-25 22:24:51 -0400

    Adding support for closing comments on other control structures

--- Sniffs/Commenting/ClosingDeclarationCommentSniff.php
+++ Sniffs/Commenting/ClosingDeclarationCommentSniff.php
@@ -33,8 +33,6 @@
  */
 class GigaOM_Sniffs_Commenting_ClosingDeclarationCommentSniff implements PHP_CodeSniffer_Sniff
 {
-
-
 	/**
 	 * Returns an array of tokens this test wants to listen for.
 	 *
@@ -46,10 +44,16 @@ class GigaOM_Sniffs_Commenting_ClosingDeclarationCommentSniff implements PHP_Cod
 			T_FUNCTION,
 			T_CLASS,
 			T_INTERFACE,
+			T_IF,
+			T_ELSE,
+			T_ELSEIF,
+			T_FOREACH,
+			T_FOR,
+			T_WHILE,
+			T_SWITCH,
 		);
 
 	}//end register
-
 
 	/**
 	 * Processes this test, when one of its tokens is encountered.
@@ -71,55 +75,85 @@ class GigaOM_Sniffs_Commenting_ClosingDeclarationCommentSniff implements PHP_Cod
 			// Abstract methods do not require a closing comment.
 			if ($methodProps['is_abstract'] === true) {
 				return;
-			}
+			}//end if
 
 			// Closures do not require a closing comment.
 			if ($methodProps['is_closure'] === true) {
 				return;
-			}
+			}//end if
 
 			// If this function is in an interface then we don't require
 			// a closing comment.
 			if ($phpcsFile->hasCondition($stackPtr, T_INTERFACE) === true) {
 				return;
-			}
+			}//end if
 
 			if (isset($tokens[$stackPtr]['scope_closer']) === false) {
 				$error = 'Possible parse error: non-abstract method defined as abstract';
 				$phpcsFile->addWarning($error, $stackPtr, 'Abstract');
 				return;
-			}
+			}//end if
 
 			$decName = $phpcsFile->getDeclarationName($stackPtr);
 			$comment = '//end '.$decName;
 			$comment_alt = '// end '.$decName;
-		} else if ($tokens[$stackPtr]['code'] === T_CLASS) {
+		}//end if
+		elseif ($tokens[$stackPtr]['code'] === T_CLASS) {
 			$comment = '//end class';
 			$comment_alt = '// end class';
-		} else {
+		}//end elseif
+		elseif ( $tokens[$stackPtr]['code'] === T_INTERFACE ) {
 			$comment = '//end interface';
 			$comment_alt = '// end interface';
-		}//end if
+		}//end elseif
+		elseif ( $tokens[$stackPtr]['code'] === T_IF ) {
+			$comment = '//end if';
+			$comment_alt = '// end if';
+		}//end elseif
+		elseif ( $tokens[$stackPtr]['code'] === T_ELSE ) {
+			$comment = '//end else';
+			$comment_alt = '// end else';
+		}//end elseif
+		elseif ( $tokens[$stackPtr]['code'] === T_ELSEIF ) {
+			$comment = '//end elseif';
+			$comment_alt = '// end elseif';
+		}//end elseif
+		elseif ( $tokens[$stackPtr]['code'] === T_WHILE ) {
+			$comment = '//end while';
+			$comment_alt = '// end while';
+		}//end elseif
+		elseif ( $tokens[$stackPtr]['code'] === T_FOR ) {
+			$comment = '//end for';
+			$comment_alt = '// end for';
+		}//end elseif
+		elseif ( $tokens[$stackPtr]['code'] === T_FOREACH ) {
+			$comment = '//end foreach';
+			$comment_alt = '// end foreach';
+		}//end elseif
+		elseif ( $tokens[$stackPtr]['code'] === T_SWITCH ) {
+			$comment = '//end switch';
+			$comment_alt = '// end switch';
+		}//end elseif
 
 		if (isset($tokens[$stackPtr]['scope_closer']) === false) {
 			$error = 'Possible parse error: %s missing opening or closing brace';
 			$data  = array($tokens[$stackPtr]['content']);
 			$phpcsFile->addWarning($error, $stackPtr, 'MissingBrace', $data);
 			return;
-		}
+		}//end if
 
 		$closingBracket = $tokens[$stackPtr]['scope_closer'];
 
 		if ($closingBracket === null) {
 			// Possible inline structure. Other tests will handle it.
 			return;
-		}
+		}//end if
 
 		$error = 'Expected '.$comment;
 		if (isset($tokens[($closingBracket + 1)]) === false || ( $tokens[($closingBracket + 1)]['code'] !== T_COMMENT && $tokens[($closingBracket + 2)]['code'] !== T_COMMENT )) {
 			$phpcsFile->addError($error, $closingBracket, 'Missing');
 			return;
-		}
+		}//end if
 
 		if (
 			   rtrim($tokens[($closingBracket + 1)]['content']) !== $comment
@@ -129,7 +163,6 @@ class GigaOM_Sniffs_Commenting_ClosingDeclarationCommentSniff implements PHP_Cod
 		) {
 			$phpcsFile->addError($error, $closingBracket, 'Incorrect');
 			return;
-		}
-
+		}//end if
 	}//end process
 }//end class
